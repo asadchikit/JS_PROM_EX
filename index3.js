b816d31dcfcf5a841c859bd80c9cc47a27272a15
asadchikit <mirbabaevsolihiddin@gmail.com>
2026-03-28 14:55:53 +0600

    первый коммит

--- index3.js
+++ index3.js
@@ -1,19 +1,3 @@
-// function getDiscount(userType, price) {
-//     if (price < 0) throw new Error("Narx manfiy bo'lishi mumkin emas");
-
-//     if (!userType) userType = "guest";
-
-//     let discount = 0;
-//     if (userType === "admin") discount = 0.3;
-//     else if (userType === "member") discount = 0.15;
-
-//     return price - price * discount;
-//   }
-
-//   console.log(getDiscount("admin", 100));
-//   console.log(getDiscount("member", 100));
-//   console.log(getDiscount("guest", 100));
-//   console.log(getDiscount(null, 100));
 
  function getDiscount(userType ,price){
     if(price < 0 ) throw new Error("manfi son")
@@ -26,3 +10,7 @@
     else if (userType === "member") discount = 0.15;
     return price 
  }
+ 
+  console.log(getDiscount("admin", 100));
+  console.log(getDiscount("member", 100));
+  console.log(getDiscount("guest", 100));
